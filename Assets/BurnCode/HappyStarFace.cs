c67f2d9c05eb05eabf0e5aaa48b23b74e58022e6
Grash <grashuriza@gmail.com>
2019-02-13 15:00:12 -0500

    Updates...

--- Assets/BurnCode/HappyStarFace.cs
+++ Assets/BurnCode/HappyStarFace.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HappyStarFace : Pickup
 {
     public AudioClip Sound;
-    AudioSource Deathsource; 
+    protected AudioSource Deathsource; 
     public override void Start()
     {
         base.Start();
